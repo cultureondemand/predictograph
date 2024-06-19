d370be2b2da495caf3e60734ee8a7f52a92c7e85
Eric C Schoenborn <cultureondemand@gmail.com>
2024-06-19 13:01:14 -0400

    Add files via upload

--- data/line-data.js
+++ data/line-data.js
@@ -1,15 +1,15 @@
 const lineData = [{
     values: [
-        {date: "2024", total: 1},
-        {date: "2025", total: 2},
-        {date: "2026", total: 4},
-        {date: "2027", total: 8},
-        {date: "2028", total: 16},
-        {date: "2029", total: 32},
-        {date: "2030", total: 64},
-        {date: "2031", total: 128},
-        {date: "2032", total: 256},
-        {date: "2033", total: 512},
+        {date: "2024/01/01", total: 1},
+        {date: "2025/01/01", total: 2},
+        {date: "2026/01/01", total: 4},
+        {date: "2027/01/01", total: 8},
+        {date: "2028/01/01", total: 16},
+        {date: "2029/01/01", total: 32},
+        {date: "2030/01/01", total: 64},
+        {date: "2031/01/01", total: 128},
+        {date: "2032/01/01", total: 256},
+        {date: "2033/01/01", total: 512},
     ]
 }]
 
